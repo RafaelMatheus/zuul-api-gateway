1ae20885424c777c0a41abe553489980e9fb3bb4
Rafael Castro <rafaelmp@ciandt.com>
2022-01-11 14:18:46 -0300

    commit inicial

---
+++ src/main/java/br/com/picpay/wallet/zuulapigetway/ZuulApiGetwayApplication.java
@@ -0,0 +1,13 @@
+package br.com.picpay.wallet.zuulapigetway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulApiGetwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApiGetwayApplication.class, args);
+    }
+
+}
